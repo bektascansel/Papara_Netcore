59d49ecbb5c2206078ef034f7be691b0b670057a
cansel bektaş <cansel.bektas6447@gmail.com>
2022-11-30 20:40:55 +0300

    CRUD

---
+++ CanselBektas-Week1-Homework/Model/Owner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CanselBektas_Week1_Homework.Model
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Date { get; set; }
+
+        public string Comment { get; set; }
+        public string  Type { get; set; }
+
+
+    }
+}
